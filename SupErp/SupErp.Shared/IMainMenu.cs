e40ab015bb028338555c95c7f2af234b5be2115b
LucasLibis <lucas.libis@hotmail.fr>
2014-10-30 14:12:33 +0100

    - Création de la solution - Ajout du projet Shared et des deux interfaces IMainMenu et ISubMenu

---
+++ SupErp/SupErp.Shared/IMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupErp.Shared
+{
+    /// <summary>
+    /// Pemrmet de définir le menu principal de votre module
+    /// Ne doit être implémenté que par une seule classe par module
+    /// </summary>
+    public interface IMainMenu
+    {
+        /// <summary>
+        /// Nom du menu principal qui sera affiché dans l'application
+        /// </summary>
+        string MenuName { get; set; }
+
+        /// <summary>
+        /// Liste des sous-menus du menu principal
+        /// </summary>
+        List<ISubMenu> SubMenus { get; set; }
+    }
+}
---
+++ SupErp/SupErp.Shared/ISubMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupErp.Shared
+{
+    /// <summary>
+    /// ¨Permet de définir qu'un élément est un sous-menu
+    /// </summary>
+    public interface ISubMenu
+    {
+        /// <summary>
+        /// Nom du sous-menu qui sera affiché dans l'application
+        /// </summary>
+        string SubMenuName { get; }
+
+        /// <summary>
+        /// Liste des sous-menus de ce sous-menu
+        /// </summary>
+        List<ISubMenu> SubMenus { get; set; }
+    }
+}
