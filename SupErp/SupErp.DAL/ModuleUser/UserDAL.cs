2ac0254a77917d8a6d5d82ea9dfa4cc6e328677a
peBruneval <ez.bruneval@gmail.com>
2014-11-13 16:13:05 +0100

    Creation dal User + requete getUserById

---
+++ SupErp/SupErp.DAL/ModuleUser/UserDAL.cs
@@ -0,0 +1,20 @@
+﻿using SupErp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupErp.DAL.ModuleUser
+{
+    public class UserDAL
+    {
+        public User GetUserById(int id)
+        {
+            using(SUPERPEntities context = new SUPERPEntities())
+            {
+                return context.Users.Find(id);
+            }
+        }
+    }
+}
