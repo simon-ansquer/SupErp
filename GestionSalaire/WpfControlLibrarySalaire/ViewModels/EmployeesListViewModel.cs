05e93bd6b0342c044960a32cb1ffa4f162844321
sansquer <simon.ansquer@gmail.com>
2014-11-20 16:22:08 +0100

    [ViewModel] Lien View EmployeesList -> ViewModel

---
+++ GestionSalaire/WpfControlLibrarySalaire/ViewModels/EmployeesListViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfControlLibrarySalaire.Helpers;
+
+namespace WpfControlLibrarySalaire.ViewModels
+{
+    public class EmployeesListViewModel : BaseViewModel
+    {
+        private List<>
+
+        public EmployeesListViewModel()
+        {
+
+        }
+
+        #region Commands
+        public ICommand SearchCommand { get { return new DelegateCommand(OnSearchButtonClick); } }
+        public ICommand GeneratePDFCommand { get { return new DelegateCommand(OnGeneratePDFClick); } } 
+        #endregion
+
+        #region Command Handlers
+        private void OnGeneratePDFClick()
+        {
+
+        }
+
+        private void OnSearchButtonClick()
+        {
+            
+        } 
+        #endregion
+    }
+}
