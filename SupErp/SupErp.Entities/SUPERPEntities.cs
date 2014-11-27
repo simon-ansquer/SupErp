4ca3acbb9a7ba2c681cc358a394740de7e226e1c
Lushkin <roman_lushkin@hotmail.fr>
2014-11-27 17:58:48 +0100

    [DAL] Désactifation des proxy d'EntityFramework

---
+++ SupErp/SupErp.Entities/SUPERPEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupErp.Entities
+{
+    public partial class SUPERPEntities
+    {
+        public SUPERPEntities(bool enableProxy)
+            : base("name=SUPERPEntities")
+        {
+            Configuration.LazyLoadingEnabled = enableProxy;
+            Configuration.ProxyCreationEnabled = enableProxy;
+        }
+    }
+}
--- SupErp/SupErp.IHM/ViewModels/MainWindowViewModel.cs
+++ SupErp/SupErp.IHM/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,16 @@
-﻿using SupErp.IHM.Helpers;
+﻿using System.Collections;
+using System.Windows.Controls;
+using SupErp.IHM.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using SupErp.IHM.ServiceReference1;
+using SupErp.Kernel;
+using SupErp.Shared;
+using SupErp.Entities;
+
 
 namespace SupErp.IHM.ViewModels
 {
@@ -95,17 +100,26 @@ namespace SupErp.IHM.ViewModels
 
 
         #region Commands
-        public void ConnectCommand(object obj) {Connect();}
+
+        public void ConnectCommand(object obj)
+        {
+            var passwordBox = obj as PasswordBox;
+            if (passwordBox != null) password = passwordBox.Password;
+            Connect();
+        }
         #endregion
 
 
 
         #region Methodes
 
-        public User Connect()
+        public void Connect()
         {
-            User user = WCFManager.UserServiceClient.Login(login, password);
-            return user;
+            
+            var user = WCFManager.UserServiceClient.Login(login, password);
+
+            DllManager dllManager = new DllManager();
+            IEnumerable<IMainMenu> mainMenus = dllManager.GetMainMenus(user.Role);
         }
         #endregion
     }
