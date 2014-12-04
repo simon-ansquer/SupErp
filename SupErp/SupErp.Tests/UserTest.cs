ce10593b0c4eee757ef69f20bbfce79e7230ccd6
dmoro33 <moro.damien@gmail.com>
2014-12-04 17:27:15 +0100

    .

--- SupErp/SupErp.Tests/UserTest.cs
+++ SupErp/SupErp.Tests/UserTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SupErp.WCF.ModuleUser;
+using SupErp.WCF;
 using SupErp.Entities;
 using System.Collections.Generic;
 using System.Linq;
