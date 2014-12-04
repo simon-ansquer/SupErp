98fd51d52ba3232cfede401b04bd5ced29a7ebe3
Thibaud Cutullic <tcutullic@gmail.com>
2014-12-04 16:33:05 +0100

    réparation conflits

--- SupErp/SupErp.Tests/UserTest.cs
+++ SupErp/SupErp.Tests/UserTest.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SupErp.WCF.ModuleUser;
 using SupErp.Entities;
 using System.Collections.Generic;
 using System.Linq;
+using SupErp.WCF;
 
 namespace SupErp.Tests
 {
