dca2bd476f7476d0bf41da1d6b6c79728fc787c0
Thibaud Cutullic <tcutullic@gmail.com>
2014-12-04 15:55:38 +0100

    compta bll view

---
+++ SupErp/SupErp.BLL/ComptabilityBLL/PlanComptableBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SupErp.BLL.ComptabilityBLL
+{
+    public class PlanComptableBLL
+    {
+        public List<List<string>> GetPlanComptable()
+        {
+
+
+            return null;
+        }
+    }
+}
