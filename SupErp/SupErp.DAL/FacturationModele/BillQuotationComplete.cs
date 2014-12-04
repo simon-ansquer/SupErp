43cc8364c227545131bcb8c1fda67a095434972e
Ruiz Coralie <ruiz.coralie64@live.fr>
2014-12-04 14:57:16 +0100

    Model Facturation en DataContract

--- SupErp/SupErp.DAL/FacturationModele/BillQuotationComplete.cs
+++ SupErp/SupErp.DAL/FacturationModele/BillQuotationComplete.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SupErp.Entities;
+using System.Runtime.Serialization;
 
 namespace SupErp.DAL.FacturationModele
 {
+    [DataContract(IsReference = true)]
+    [KnownType(typeof(BillQuotationLight))]
+    [KnownType(typeof(BILL_LineBillQuotation))]
     public class BillQuotationComplete : BillQuotationLight
     {
+        [DataMember]
         public List<BILL_LineBillQuotation> lines { get; private set; }
 
         public BillQuotationComplete(BILL_BillQuotation bill_billQuotation): base(bill_billQuotation)
