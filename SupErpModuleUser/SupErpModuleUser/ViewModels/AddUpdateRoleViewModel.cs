37a70f9f2191a1bab9a8de74134fc8e0abd7a88d
peBruneval <ez.bruneval@gmail.com>
2014-12-07 23:27:19 +0100

    Role en cours

--- SupErpModuleUser/SupErpModuleUser/ViewModels/AddUpdateRoleViewModel.cs
+++ SupErpModuleUser/SupErpModuleUser/ViewModels/AddUpdateRoleViewModel.cs
@@ -38,18 +38,32 @@ namespace SupErpModuleUser.ViewModels
         public AddUpdateRoleViewModel(int roleId)
         {
             //Role = role.ToIHMRole();
-            Role = new IHMRole();
-            Role.Label = "Test";
-            //Modules = new UserService.UserServiceClient().GetModules();
+            //Role = new IHMRole();
+            //Role.Label = "Test";
+            ////Modules = new UserService.UserServiceClient().GetModules();
 
-            IHMModule module1 = new IHMModule();
-            module1.Name = "Utilisateur";
-            IHMModule module2 = new IHMModule();
-            module2.Name = "Salaire";
+            //IHMModule module1 = new IHMModule();
+            //module1.Name = "Utilisateur";
+            //IHMModule module2 = new IHMModule();
+            //module2.Name = "Salaire";
 
-            Modules = new List<IHMModule>();
-            Modules.Add(module1);
-            Modules.Add(module2);
+            //Modules = new List<IHMModule>();
+            //Modules.Add(module1);
+            //Modules.Add(module2);
+
+            using(UserServiceClient ws = new UserServiceClient())
+            {
+                var rep = ws.GetRoleById(roleId);
+                if (rep != null)
+                {
+                    Role = rep.ToIHMRole();
+                    if (Role.Modules != null)
+                        Modules = Role.Modules.ToList();
+                    else
+                        Modules = new List<IHMModule>();
+                }
+                Role = new IHMRole();
+            }
         }
 
         #region Commands
--- SupErpModuleUser/SupErpModuleUser/Views/PageSwitcher.xaml.cs
+++ SupErpModuleUser/SupErpModuleUser/Views/PageSwitcher.xaml.cs
@@ -25,7 +25,7 @@ namespace SupErpModuleUser.Views
         {
             InitializeComponent();
             Switcher.PageSwitcher = this;
-            Switcher.Switch(new ListUserUserControl());
+            Switcher.Switch(new ListRoleUserControl());
         }
 
         internal void Navigate(UserControl goToUserControl)
