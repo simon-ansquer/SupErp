d3012b15515d751b77172757f985de238721fc8f
LucasLibis <lucas.libis@hotmail.fr>
2014-12-08 23:24:48 +0100

    Correction du commit qui a supprimé l'IHM !

---
+++ SupErp/SupErp.IHM/Models/WindowParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupErp.IHM.Models
+{
+    public class WindowParameters
+    {
+
+        public double ScreenWidth { get; set; }
+        public double ScreenHeight { get; set; }
+
+    }
+}
