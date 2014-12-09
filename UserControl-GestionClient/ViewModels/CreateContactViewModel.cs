11d5faea3b58ffa2037b41d5dc97bf3e6eeb285f
aurelienMerzeau17 <aurelien.merzeau@gmail.com>
2014-12-09 19:06:05 +0100

    mise en place des grids sur la page de création de clients

--- UserControl-GestionClient/ViewModels/CreateContactViewModel.cs
+++ UserControl-GestionClient/ViewModels/CreateContactViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using UserControl_GestionClient.Helpers;
+using UserControl_GestionClient.Models;
 
 namespace UserControl_GestionClient.ViewModels
 {
     class CreateContactViewModel
     {
+        public Company_Contact CreateContact { get; set; }
+        public ICommand AddCustomer { get { return new DelegateCommand(AddNewCustomer); } }
+        private void AddNewCustomer()
+        {
+
+        }
     }
 }
