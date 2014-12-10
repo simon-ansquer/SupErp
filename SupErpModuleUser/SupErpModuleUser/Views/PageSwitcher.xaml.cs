5e64125efb94a1787d7a8a76aab8a5c0e7108790
Eliott <eliott.lujan.biig@gmail.com>
2014-12-10 18:59:00 +0100

    Remise en place du UserControl par défaut

--- SupErpModuleUser/SupErpModuleUser/Views/PageSwitcher.xaml.cs
+++ SupErpModuleUser/SupErpModuleUser/Views/PageSwitcher.xaml.cs
@@ -25,7 +25,7 @@ namespace SupErpModuleUser.Views
         {
             InitializeComponent();
             Switcher.PageSwitcher = this;
-            Switcher.Switch(new AddUpdateUserUserControl());
+            Switcher.Switch(new ListUserUserControl());
         }
 
         internal void Navigate(UserControl goToUserControl)
